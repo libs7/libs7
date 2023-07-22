7df0ab9840e3ecd608dce32fe953ea89667f0301
Gregg Reynolds <601396+mobileink@users.noreply.github.com>
2023-07-22 10:05:04 -0500

    rm test stuff that has migrated to separate repos
    * libs7/utf8proc_s7, mustachios batch tests to libs/s7x

--- test/batch/mustache/case002/colors.scm
+++
@@ -1,6 +0,0 @@
-((:header . "Colors")
- (:items . #(
-           ((:name . "red")   (:first . #t) (:url . "#Red"))
-           ((:name . "green") (:link  . #t) (:url . "#Green"))
-           ((:name . "blue")  (:link  . #t) (:url . "#Blue"))))
- (:empty . #f))
